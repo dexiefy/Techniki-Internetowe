f5a16371fe739c71d6fd0f8c9b433d80a30433b5
dexiefy <pdziegielewski@sigma.ug.edu.pl>
2017-03-24 15:54:43 +0100

    Update gulpfile.js

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -1,5 +1,20 @@
-var gulp = require('gulp');
-
-gulp.task('default', function() {
-  // place code for your default task here
+var gulp = require('gulp'),
+  connect = require('gulp-connect');
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: 'docs',
+    livereload: true
+  });
 });
+ 
+gulp.task('html', function () {
+  gulp.src('./docs/*.html')
+    .pipe(connect.reload());
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch(['./docs/*.html'], ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch']);
