ed57cabbcae84b637151d35f88e820c00f857ca5
dexiefy <dexiefy@gmail.com>
2017-05-05 20:16:02 +0200

    responsive images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect');
- 
+  var responsive = require('gulp-responsive');
+
+
+gulp.task('default', ['connect', 'watch', 'images']);
+
 gulp.task('connect', function() {
   connect.server({
     root: 'docs',
@@ -17,4 +21,35 @@ gulp.task('watch', function () {
   gulp.watch(['./docs/*.html'], ['html']);
 });
  
-gulp.task('default', ['connect', 'watch']);
+
+gulp.task('images', function () {
+  return gulp.src('./docs/Assets/Images/*.{jpg,png}')
+    .pipe(responsive({
+      // Convert all images to JPEG format
+      '*': [{
+        // image-medium.jpg is 375 pixels wide
+        width: 360,
+        rename: {
+          suffix: '-small',
+          extname: '.jpg',
+        },
+      }, {
+        // image-large.jpg is 480 pixels wide
+        width: 720,
+        rename: {
+          suffix: '-medium',
+          extname: '.jpg',
+        },
+      }, {
+        // image-extralarge.jpg is 768 pixels wide
+        width: 1080,
+        rename: {
+          suffix: '-large',
+          extname: '.jpg',
+        },
+      }],
+    }))
+    .pipe(gulp.dest('./docs/Assets/Images'));
+});
+
+
